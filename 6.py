a957339cea589d4af42c9277f5cc987d18ab0deb
Henrique de Cerqueira <henriquecorrealima@hotmail.com>
2023-10-04 18:27:05 -0300

    Pois é

--- 6.py
+++ 6.py
@@ -1,2 +1,6 @@
-convidados= str['Dwayne Johnson', 'Kanye West', 'Ozzy Ousborne', 'Naldo Benny','Ronaldinho']
-print('Você esta convidado' [0])
+convidados= ['Manoel Gomes', 'Danilo Gentili', 'Supla', 'Joe Biden','Bolsonaro']
+print('Você esta convidado ' + str(convidados[0]))
+print('Você esta convidado ' + str(convidados[1]))
+print('Você esta convidado ' + str(convidados[2]))
+print('Você esta convidado ' + str(convidados[3]))
+print('Você esta convidado ' + str(convidados[4]))
